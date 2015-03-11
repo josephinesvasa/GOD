3c3f863da38850b277f7ba46ccd381f3a6334f6a
Elifro <elias.frosell@gmail.com>
2015-03-11 12:01:52 +0100

    En start
    Varför visas användarnamnet under profilbilden?
    
    Med Vänliga Hälsningar
    Elias Frösell
    0706852646

--- index.php
+++ index.php
@@ -1,4 +1,26 @@
-<?php
-/**
-test
- */
+<html>
+<head>
+    <title>Hello World</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div id="container" class="col-md-16">
+    <div id="header" class="col-md-16">
+        <div id="user_container" class="col-md-3">
+            <div id="user_image" class="col-md-6" style="background-image: url('http://burrilldigitalhealth.com/wp-content/uploads/bw_square_profile_picture_1800x1800_medium_res.jpeg');"></div>
+            <div id="user_name" class="col-md-10">Lars Åhli</div>
+        </div>
+        <div id="tabs" class="col-md-7"></div>
+        <div id="menu_bars" class="col-md-2"><i class="fa fa-bars"></i></div>
+    </div>
+
+
+</div>
+
+</body>
+</html>
